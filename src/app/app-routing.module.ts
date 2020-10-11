495e7ecd37ebc567ad9537d284cd660b1461761a
Adolfo <adolfo_zuazo@hotmail.com>
2020-10-11 17:44:44 -0300

    Se agrego estilos.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/components/login/login.component';
 import { RegisterComponent } from './auth/components/register/register.component';
-import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {
